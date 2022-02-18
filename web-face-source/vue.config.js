524ca7577c340ba3f912105588711cdf0ef5120a
Ivan <lih1989@yandex.ru>
2022-02-18 16:20:58 +0300

    Изменение пути сборки веб интерфейса

--- web-face-source/vue.config.js
+++ web-face-source/vue.config.js
@@ -1,4 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+
 module.exports = {
+  outputDir: path.resolve(__dirname, '../NightLight/data'),
   devServer: {
     // host: 'crm-local.anflat.ru',
     port: 7777
