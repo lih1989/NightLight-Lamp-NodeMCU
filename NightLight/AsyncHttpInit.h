dee1db9b173707aa93b7ffd16cd28ab449c244c2
Ivan <lih1989@yandex.ru>
2022-03-10 15:37:57 +0300

    Новый веб сервер

--- NightLight/AsyncHttpInit.h
+++ NightLight/AsyncHttpInit.h
@@ -14,28 +14,54 @@
 
 AsyncWebServer server(80);
 
-const char* PARAM_MESSAGE = "message";
+// Шаблон для редиректа
+const String metaRefreshStr = "<head><meta http-equiv=\"refresh\" content=\"0; url=http://" + apIP.toString() + "\" /></head><body><p>redirecting...</p></body>";
+
+class CaptiveRequestHandler : public AsyncWebHandler {
+public:
+  CaptiveRequestHandler() {}
+  virtual ~CaptiveRequestHandler() {}
+
+  bool canHandle(AsyncWebServerRequest *request){
+    Serial.println("---canHandle---");
+    Serial.println(apIP.toString());
+    Serial.println(String(request->host()));
+    //request->addInterestingHeader("ANY");
+    if(apIP.toString() != String(request->host())) {
+        return true;
+    } else {
+        return false;
+    }
+  }
+
+  void handleRequest(AsyncWebServerRequest *request) {
+    Serial.println("---CaptiveRequestHandler---");
+    Serial.println(apIP.toString());
+    Serial.println(String(request->host()));
+    request->send(200, "text/html", metaRefreshStr);
+  }
+};
 
 void notFound(AsyncWebServerRequest *request) {
+    Serial.println("---notFound---");
+    Serial.println(apIP.toString());
+    Serial.println(String(request->host()));
     request->send(404, "text/plain", "Not found");
 }
 
 void initHttp(void) {
-    // Server with different default file
-    server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
-    //AND AUTH server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html").setAuthentication("user", "pass");
-
+    server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html"); //set AUTH .setAuthentication("user", "pass");
     // Serve files in directory "/www/" when request url starts with "/"
-    // Request to the root or none existing files will try to server the defualt
+    // Request to the root or none existing files will try to server the default
     // file name "index.htm" if exists
-
-    //  serveStatic on '/' responds with net::ERR_ABORTED 404 for subdirectories
+    // [WARNING] serveStatic on '/' responds with net::ERR_ABORTED 404 for subdirectories
     // FIX: Maximum 5 requests at a time!
     server.serveStatic("/img/", SPIFFS, "/img/").setCacheControl("max-age=360");
     server.serveStatic("/js/", SPIFFS, "/js/").setCacheControl("max-age=360");
     server.serveStatic("/css/", SPIFFS, "/css/").setCacheControl("max-age=360");
 
     server.onNotFound(notFound);
+    server.addHandler(new CaptiveRequestHandler()).setFilter(ON_AP_FILTER);//only when requested from AP
 
     server.begin();
     Serial.println("Async HTTP server started");
