de7fa14dcf6c0636ea0c468f87d6ccaa8d60a737
Ivan <lih1989@yandex.ru>
2022-03-11 12:31:15 +0300

    add vscode platformio source

---
+++ firmware/src/_OLD_http_init.h
@@ -0,0 +1,98 @@
+#include <ESP8266WebServer.h>
+
+ESP8266WebServer server(80);
+
+// Шаблон для редиректа
+const String metaRefreshStr = "<head><meta http-equiv=\"refresh\" content=\"0; url=http://" + apIP.toString() + "\" /></head><body><p>redirecting...</p></body>";
+
+String getContentType(String filename){
+    if(server.hasArg("download")) return "application/octet-stream";
+    else if(filename.endsWith(".htm")) return "text/html";
+    else if(filename.endsWith(".html")) return "text/html";
+    else if(filename.endsWith(".css")) return "text/css";
+    else if(filename.endsWith(".svg")) return "image/svg+xml";
+    else if(filename.endsWith(".woff")) return "application/font-woff";
+    else if(filename.endsWith(".woff2")) return "application/font-woff2";
+    else if(filename.endsWith(".ttf")) return "application/x-font-truetype";
+    else if(filename.endsWith(".eot")) return "application/vnd.ms-fontobject";
+    else if(filename.endsWith(".otf")) return "application/x-font-opentype";
+    else if(filename.endsWith(".js")) return "application/javascript";
+    else if(filename.endsWith(".png")) return "image/png";
+    else if(filename.endsWith(".gif")) return "image/gif";
+    else if(filename.endsWith(".jpg")) return "image/jpeg";
+    else if(filename.endsWith(".ico")) return "image/x-icon";
+    else if(filename.endsWith(".xml")) return "text/xml";
+    else if(filename.endsWith(".pdf")) return "application/x-pdf";
+    else if(filename.endsWith(".zip")) return "application/x-zip";
+    else if(filename.endsWith(".json")) return "text/html";
+    else if(filename.endsWith(".gz")) return "application/x-gzip";
+    return "text/plain";
+}
+
+bool handleFileRead(String path){
+     Serial.println("handleFileRead: " + path);
+    if(path.endsWith("/")) path += "index.html";
+    String contentType = getContentType(path);
+     Serial.println("handleFileRead Content: " + contentType);
+    String pathWithGz = path + ".gz";
+    if(SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)){
+        if(SPIFFS.exists(pathWithGz))
+            path += ".gz";
+        File file = SPIFFS.open(path, "r");
+        size_t sent = server.streamFile(file, contentType);
+        file.close();
+        Serial.println("handleFileRead exists: " + path);
+        return true;
+    }
+    Serial.println("handleFileRead NOT exists");
+    return false;
+}
+
+void handleNotFound()
+{
+    // Если host обращения не сщщтвествует ip адресу устройства
+    if(apIP.toString() != String(server.hostHeader())) {
+        Serial.println("---REDIRECT---<" + apIP.toString() + "> | <" + String(server.hostHeader()) + ">");
+        server.send(200, "text/html", metaRefreshStr);
+    } else if(!handleFileRead(server.uri())) {
+        Serial.println("---File Not Found---");
+        String message = "File Not Found\n\n";
+        message += "URI: ";
+        message += server.uri();
+        message += "\nMethod: ";
+        message += (server.method() == HTTP_GET)?"GET":"POST";
+        message += "\nArguments: ";
+        message += server.args();
+        message += "\n";
+        for (uint8_t i=0; i<server.args(); i++){
+            message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+        }
+        server.send(404, "text/plain", message);
+    }
+}
+
+void handleRoot() {
+  if(!handleFileRead("/index.html")) {
+    server.send(404, "text/plain", "FileNotFound");
+  }
+}
+
+void HTTP_init(void) {
+    server.on("/", handleRoot);
+    //called when the url is not defined here
+    //use it to load content from SPIFFS
+    server.onNotFound(handleNotFound);
+    server.serveStatic("/img", SPIFFS, "/img","max-age=60");
+    server.serveStatic("/js",   SPIFFS, "/js"  ,"max-age=60");
+    server.serveStatic("/css",  SPIFFS, "/css" ,"max-age=60");
+
+    server.begin();
+    Serial.println("HTTP server started");
+//    TODO DEBUG
+//    ConfigState.data.effect = random(1, 5);
+//    ConfigState.writeFileState();
+//    char tmp[256];
+//    Serial.println("PRINT:");
+//    ConfigState.getState(tmp);
+//    Serial.println(tmp);
+}
---
+++ firmware/src/jsonStateHelper.h
@@ -0,0 +1,192 @@
+#include <ArduinoJson.h>
+#include <FS.h>
+
+class JsonState {
+  public:
+    struct {
+      bool status;
+      bool motionSensor;
+      long motionSensorSeconds;
+      int effect;
+      char ssidAP[64];
+      char passwordAP[64];
+      char ssid[64];
+      char password[64];
+      int volume;
+    } data;
+
+    JsonState () {
+      SPIFFS.begin();
+    };
+
+    // Инициализировать состояние хранилища из файла
+    void initReadState (const char* stateFilePath) {
+      // Open file for reading
+      File file = SPIFFS.open(stateFilePath, "r");
+      if (!file) {
+        Serial.println(F("loadConfiguration - Failed to open file"));
+        return;
+      }
+
+      // Allocate a temporary JsonDocument
+      // Don't forget to change the capacity to match your requirements.
+      // Use https://arduinojson.org/v6/assistant to compute the capacity.
+      StaticJsonDocument<512> doc;
+
+      // Deserialize the JSON document
+      DeserializationError error = deserializeJson(doc, file);
+      if (error){
+        Serial.println(F("loadConfiguration - deserializeJson: Failed to read file, using default configuration"));
+        return;
+      }
+
+      // Copy values from the JsonDocument to the State Data
+      data.status = doc["status"] | false;
+      data.motionSensor = doc["motionSensor"] | false;
+      data.motionSensorSeconds = doc["motionSensorSeconds"] | 3600;
+      data.effect = doc["effect"] | 1;
+      data.volume = doc["volume"] | 10;
+      strlcpy(data.ssidAP, doc["ssidAP"], sizeof(data.ssidAP));
+      strlcpy(data.passwordAP, doc["passwordAP"], sizeof(data.passwordAP));
+      strlcpy(data.ssid, doc["ssid"], sizeof(data.ssid));
+      strlcpy(data.password, doc["password"], sizeof(data.password));
+      // Serial.printf("LOAD: [data.ssidAP:%s], [doc[ssidAP]:%s]\n", data.ssidAP,  doc["ssidAP"]);
+
+      // Close the file (Curiously, File's destructor doesn't close the file)
+      file.close();
+    }
+
+    // Сохранить состояние хранилища в файл
+    void writeFileState (const char* stateFilePath) {
+      // Serial.println("--- writeFileState---START---");
+      // Serial.printf("[ssidAP:%s], [passwordAP:%s], [ssid:%s], [password:%s]\n", data.ssidAP,  data.passwordAP, data.ssid, data.password);
+
+      StaticJsonDocument<512> doc;
+
+      // Set the values in the document
+      doc["status"] = data.status;
+      doc["motionSensor"] = data.motionSensor;
+      doc["motionSensorSeconds"] = data.motionSensorSeconds;
+      doc["effect"] = data.effect;
+      doc["volume"] = data.volume;
+      doc["ssidAP"] = data.ssidAP;
+      doc["passwordAP"] = data.passwordAP;
+      doc["ssid"] = data.ssid;
+      doc["password"] = data.password;
+
+      // Serialize JSON to file
+      char strData[512];
+      if (serializeJsonPretty(doc, strData) == 0) {
+        Serial.println(F("writeFileState - serializeJsonPretty: Failed to write to file"));
+      }
+      // Serial.println("+++writeFileState - strData+++");
+      // Serial.println(strData);
+
+
+      Serial.printf("Writing file: %s\r\n", stateFilePath);
+
+      File file = SPIFFS.open(stateFilePath, "w");
+      if(!file){
+          Serial.println("- failed to open file for writing");
+          return;
+      }
+      if(file.print(strData)){
+          Serial.println("- file written");
+      } else {
+          Serial.println("- write failed");
+      }
+      file.close();
+      Serial.println("--- writeFileState---END---");
+    }
+
+    // метод читает состояние с файловой системы
+    void readFileState (const char* stateFilePath, char* return_data) {
+        // Serial.printf("Reading file: %s\n", stateFilePath);
+        File file = SPIFFS.open(stateFilePath, "r");
+        if (!file || file.isDirectory()) {
+            Serial.println("Failed to open file for reading");
+            return;
+        }
+        // Serial.print("Read from file: ");
+        char tmp[file.size() + 1];
+        int n=0;
+        while (file.available()) {
+            char c =  file.read();
+            delayMicroseconds(100);
+            tmp[n] = c; //записываю во временный буфер
+            n=n+1;
+        }
+        file.close();
+
+        strlcpy(return_data, tmp, sizeof(tmp));
+    }
+
+    // метод читает состояние из структуры
+    void getState (char* output) {
+      StaticJsonDocument<512> doc;
+
+      // Set the values in the document
+      doc["status"] = data.status;
+      doc["motionSensor"] = data.motionSensor;
+      doc["motionSensorSeconds"] = data.motionSensorSeconds;
+      doc["effect"] = data.effect;
+      doc["volume"] = data.volume;
+      doc["ssidAP"] = data.ssidAP;
+      doc["passwordAP"] = data.passwordAP;
+      doc["ssid"] = data.ssid;
+      doc["password"] = data.password;
+
+      char tmp[512];
+      if (serializeJsonPretty(doc, tmp) == 0) {
+        Serial.println(F("getState - serializeJsonPretty: Failed to write to file"));
+      }
+      strlcpy(output, tmp, sizeof(tmp));
+    }
+
+    // Изменить состояние хранилища в соотвествии с данными веб сокетов
+    void wsJsonPayloadHandler(char * payload) {
+      // Serial.println(payload);
+      StaticJsonDocument<512> doc;
+      DeserializationError error = deserializeJson(doc, payload);
+      if (error) {
+        Serial.println(F("wsJsonPayloadHandler - wsJsonPayloadHandler: Failed to read"));
+        return;
+      }
+
+      if (doc.containsKey("status")) {
+        data.status = doc["status"];
+      }
+
+      if (doc.containsKey("motionSensor")) {
+        data.motionSensor = doc["motionSensor"];
+      }
+
+      if (doc.containsKey("motionSensorSeconds")) {
+        data.motionSensorSeconds = doc["motionSensorSeconds"];
+      }
+
+      if (doc.containsKey("effect")) {
+        data.effect = doc["effect"];
+      }
+
+      if (doc.containsKey("volume")) {
+        data.volume = doc["volume"];
+      }
+
+      if (doc.containsKey("ssidAP")) {
+        strlcpy(data.ssidAP, doc["ssidAP"], sizeof(data.ssidAP));
+      }
+
+      if (doc.containsKey("passwordAP")) {
+        strlcpy(data.passwordAP, doc["passwordAP"], sizeof(data.passwordAP));
+      }
+
+      if (doc.containsKey("ssid")) {
+        strlcpy(data.ssid, doc["ssid"], sizeof(data.ssid));
+      }
+
+      if (doc.containsKey("password")) {
+        strlcpy(data.password, doc["password"], sizeof(data.password));
+      }
+    }
+};
