14257679f5191db0c62b0109829f48a339f2954f
Ivan <lih1989@yandex.ru>
2022-03-11 14:04:47 +0300

    remove old + replace web build path

--- web-face-source/vue.config.js
+++ web-face-source/vue.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 module.exports = {
   productionSourceMap: false,
-  outputDir: path.resolve(__dirname, '../NightLight/data'),
+  outputDir: path.resolve(__dirname, '../firmware/data'),
   publicPath: '',
   filenameHashing: false,
   devServer: {
