87d8c54c6863031849045b5c605dc74a2ff908c9
Иван <lih1989@yandex.ru>
2022-03-13 18:29:28 +0300

    Settings Frontend for VS code

--- web-face-source/src/shims-vue.d.ts
+++ web-face-source/src/shims-vue.d.ts
@@ -1,7 +1,5 @@
 /* eslint-disable */
 
-declare module 'vue-native-websocket-vue3';
-
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
