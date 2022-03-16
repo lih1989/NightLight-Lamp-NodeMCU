56bee3badfb9951cea6c8065dcfd78a2ef2c5f2b
Ivan <lih1989@yandex.ru>
2022-03-16 13:45:24 +0300

    Коммент излишков

--- firmware/src/rgbManager.h
+++ firmware/src/rgbManager.h
@@ -1,6 +1,6 @@
 #include <FastLED.h>
-#include "GyverButton.h"
-#include "GyverTimer.h"
+// #include "GyverButton.h"
+// #include "GyverTimer.h"
 
 // ************************** НАСТРОЙКИ ***********************
 #define CURRENT_LIMIT 2000  // лимит по току в миллиамперах, автоматически управляет яркостью (пожалей свой блок питания!) 0 - выключить лимит
@@ -18,30 +18,30 @@
 #define MODES_AMOUNT 6
 
 // #include "GyverButton.h"
-GButton touch(BTN_PIN, LOW_PULL, NORM_OPEN);
+// GButton touch(BTN_PIN, LOW_PULL, NORM_OPEN);
 
 // #include <FastLED.h>
 CRGB leds[NUM_LEDS];
-CRGBPalette16 gPal;
+// CRGBPalette16 gPal;
 
 // #include "GyverTimer.h"
-GTimer_ms effectTimer(60);
-GTimer_ms autoplayTimer((long)AUTOPLAY_TIME * 1000);
-GTimer_ms brightTimer(20);
+// GTimer_ms effectTimer(60);
+// GTimer_ms autoplayTimer((long)AUTOPLAY_TIME * 1000);
+// GTimer_ms brightTimer(20);
 
-int brightness = BRIGHTNESS;
-int tempBrightness;
-byte thisMode;
+// int brightness = BRIGHTNESS;
+// int tempBrightness;
+// byte thisMode;
 
-bool gReverseDirection = false;
-boolean loadingFlag = true;
-boolean autoplay = true;
-boolean powerDirection = true;
-boolean powerActive = false;
-boolean powerState = true;
-boolean whiteMode = false;
-boolean brightDirection = true;
-boolean wasStep = false;
+// bool gReverseDirection = false;
+// boolean loadingFlag = true;
+// boolean autoplay = true;
+// boolean powerDirection = true;
+// boolean powerActive = false;
+// boolean powerState = true;
+// boolean whiteMode = false;
+// boolean brightDirection = true;
+// boolean wasStep = false;
 
 
 
@@ -49,7 +49,7 @@ class rgbManager {
     private:
         void fillAll(CRGB newcolor);
         uint32_t getPixColor(int thisPixel);
-        void brightnessTick();
+        // void brightnessTick();
         /* data */
     public:
         rgbManager(/* args */); // конструктор
@@ -62,17 +62,17 @@ class rgbManager {
 rgbManager::rgbManager(/* args */) {
     FastLED.addLeds<WS2811, LED_PIN, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
     if (CURRENT_LIMIT > 0) FastLED.setMaxPowerInVoltsAndMilliamps(5, CURRENT_LIMIT / NUM_STRIPS);
-    FastLED.setBrightness(brightness);
+    FastLED.setBrightness(255);
     FastLED.show();
 
-  randomSeed(analogRead(0));
-  touch.setTimeout(300);
-  touch.setStepTimeout(50);
+  // randomSeed(analogRead(0));
+  // touch.setTimeout(300);
+  // touch.setStepTimeout(50);
 
-  if (FIRE_PALETTE == 0) gPal = HeatColors_p;
-  else if (FIRE_PALETTE == 1) gPal = CRGBPalette16( CRGB::Black, CRGB::Red, CRGB::Yellow, CRGB::White);
-  else if (FIRE_PALETTE == 2) gPal = CRGBPalette16( CRGB::Black, CRGB::Blue, CRGB::Aqua,  CRGB::White);
-  else if (FIRE_PALETTE == 3) gPal = CRGBPalette16( CRGB::Black, CRGB::Red, CRGB::White);
+  // if (FIRE_PALETTE == 0) gPal = HeatColors_p;
+  // else if (FIRE_PALETTE == 1) gPal = CRGBPalette16( CRGB::Black, CRGB::Red, CRGB::Yellow, CRGB::White);
+  // else if (FIRE_PALETTE == 2) gPal = CRGBPalette16( CRGB::Black, CRGB::Blue, CRGB::Aqua,  CRGB::White);
+  // else if (FIRE_PALETTE == 3) gPal = CRGBPalette16( CRGB::Black, CRGB::Red, CRGB::White);
 }
 
 // деструктор
@@ -85,121 +85,136 @@ void rgbManager::fillAll(CRGB newcolor) {
   }
 }
 
-// функция получения цвета пикселя по его номеру
-uint32_t rgbManager::getPixColor(int thisPixel) {
-  return (((uint32_t)leds[thisPixel].r << 16) | ((long)leds[thisPixel].g << 8 ) | (long)leds[thisPixel].b);
-}
-
-void rgbManager::nextMode() {
-  thisMode++;
-  if (thisMode >= MODES_AMOUNT) thisMode = 0;
-  loadingFlag = true;
-  FastLED.clear();
-}
-
-void rgbManager::brightnessTick() {
-  if (powerActive) {
-    if (brightTimer.isReady()) {
-      if (powerDirection) {
-        powerState = true;
-        tempBrightness += 10;
-        if (tempBrightness > brightness) {
-          tempBrightness = brightness;
-          powerActive = false;
-        }
-        FastLED.setBrightness(tempBrightness);
-        FastLED.show();
-      } else {
-        tempBrightness -= 10;
-        if (tempBrightness < 0) {
-          tempBrightness = 0;
-          powerActive = false;
-          powerState = false;
-        }
-        FastLED.setBrightness(tempBrightness);
-        FastLED.show();
-      }
-    }
-  }
-}
-
 void rgbManager::loop() {
-  touch.tick();
-  if (touch.hasClicks()) {
-    byte clicks = touch.getClicks();
-    switch (clicks) {
-      case 1:
-        powerDirection = !powerDirection;
-        powerActive = true;
-        tempBrightness = brightness * !powerDirection;
-        break;
-      case 2: if (!whiteMode && !powerActive) {
-          nextMode();
-        }
-        break;
-      case 3: if (!powerActive) {
-          whiteMode = !whiteMode;
-          if (whiteMode) {
-            effectTimer.stop();
-            fillAll(CRGB::White);
-            FastLED.show();
-          } else {
-            effectTimer.start();
-          }
-        }
-        break;
-      case 4: if (!whiteMode && !powerActive) autoplay = !autoplay;
-        break;
-      default:
-        break;
-    }
-  }
-
-  if (touch.isStep()) {
-    if (!powerActive) {
-      wasStep = true;
-      if (brightDirection) {
-        brightness += 5;
-      } else {
-        brightness -= 5;
-      }
-      brightness = constrain(brightness, MIN_BRIGHTNESS, 255);
-      FastLED.setBrightness(brightness);
-      FastLED.show();
-    }
-  }
-
-  if (touch.isRelease()) {
-    if (wasStep) {
-      wasStep = false;
-      brightDirection = !brightDirection;
-    }
-  }
-
-  if (effectTimer.isReady() && powerState) {
-    Serial.println("RGB > Loop > change mode");
-    // switch (thisMode) {
-    //   case 0: lighter();
-    //     break;
-    //   case 1: lightBugs();
-    //     break;
-    //   case 2: colors();
-    //     break;
-    //   case 3: rainbow();
-    //     break;
-    //   case 4: sparkles();
-    //     break;
-    //   case 5: fire();
-    //     break;
-    //   case 6: vinigret();
-    //     break;
-    // }
-    FastLED.show();
-  }
-
-  if (autoplayTimer.isReady() && autoplay) {    // таймер смены режима
-    nextMode();
-  }
-
-  brightnessTick();
+  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  // TODO может нахер этот луп??? Запускать при socket onEventHandler или поcле при помощи Callback ????????
+  // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  // if (!powerActive) {
+  //   if (ConfigState.data.status) {
+  //     fillAll(CRGB::White);
+  //     FastLED.show();
+  //   } else {
+  //     FastLED.clear();
+  //   }
+  // }
 }
+
+// // функция получения цвета пикселя по его номеру
+// uint32_t rgbManager::getPixColor(int thisPixel) {
+//   return (((uint32_t)leds[thisPixel].r << 16) | ((long)leds[thisPixel].g << 8 ) | (long)leds[thisPixel].b);
+// }
+
+// void rgbManager::nextMode() {
+//   thisMode++;
+//   if (thisMode >= MODES_AMOUNT) thisMode = 0;
+//   loadingFlag = true;
+//   FastLED.clear();
+// }
+
+// void rgbManager::brightnessTick() {
+//   if (powerActive) {
+//     if (brightTimer.isReady()) {
+//       if (powerDirection) {
+//         powerState = true;
+//         tempBrightness += 10;
+//         if (tempBrightness > brightness) {
+//           tempBrightness = brightness;
+//           powerActive = false;
+//         }
+//         FastLED.setBrightness(tempBrightness);
+//         FastLED.show();
+//       } else {
+//         tempBrightness -= 10;
+//         if (tempBrightness < 0) {
+//           tempBrightness = 0;
+//           powerActive = false;
+//           powerState = false;
+//         }
+//         FastLED.setBrightness(tempBrightness);
+//         FastLED.show();
+//       }
+//     }
+//   }
+// }
+
+// TODO old loop
+// void rgbManager::loop() {
+//   touch.tick();
+//   if (touch.hasClicks()) {
+//     byte clicks = touch.getClicks();
+//     switch (clicks) {
+//       case 1:
+//         powerDirection = !powerDirection;
+//         powerActive = true;
+//         tempBrightness = brightness * !powerDirection;
+//         break;
+//       case 2: if (!whiteMode && !powerActive) {
+//           nextMode();
+//         }
+//         break;
+//       case 3: if (!powerActive) {
+//           whiteMode = !whiteMode;
+//           if (whiteMode) {
+//             effectTimer.stop();
+//             fillAll(CRGB::White);
+//             FastLED.show();
+//           } else {
+//             effectTimer.start();
+//           }
+//         }
+//         break;
+//       case 4: if (!whiteMode && !powerActive) autoplay = !autoplay;
+//         break;
+//       default:
+//         break;
+//     }
+//   }
+
+//   if (touch.isStep()) {
+//     if (!powerActive) {
+//       wasStep = true;
+//       if (brightDirection) {
+//         brightness += 5;
+//       } else {
+//         brightness -= 5;
+//       }
+//       brightness = constrain(brightness, MIN_BRIGHTNESS, 255);
+//       FastLED.setBrightness(brightness);
+//       FastLED.show();
+//     }
+//   }
+
+//   if (touch.isRelease()) {
+//     if (wasStep) {
+//       wasStep = false;
+//       brightDirection = !brightDirection;
+//     }
+//   }
+
+//   if (effectTimer.isReady() && powerState) {
+//     Serial.println("RGB > Loop > change mode");
+//     // switch (thisMode) {
+//     //   case 0: lighter();
+//     //     break;
+//     //   case 1: lightBugs();
+//     //     break;
+//     //   case 2: colors();
+//     //     break;
+//     //   case 3: rainbow();
+//     //     break;
+//     //   case 4: sparkles();
+//     //     break;
+//     //   case 5: fire();
+//     //     break;
+//     //   case 6: vinigret();
+//     //     break;
+//     // }
+//     FastLED.show();
+//   }
+
+//   if (autoplayTimer.isReady() && autoplay) {    // таймер смены режима
+//     nextMode();
+//   }
+
+//   brightnessTick();
+// }
